f435b97d19a13c10cd5b021043fbec4fe7fae29a
Manish <manny8080manny@gmail.com>
2023-03-08 21:54:58 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:cafeapp/coffeeui.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: CoffeeUi(),
+    );
+  }
+}
